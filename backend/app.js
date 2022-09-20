768db69213d6b38db622fc245c92e188df72f9ae
kaisadiakhate <kaisa.diakhate@awacademy.training>
2022-09-20 11:50:18 +0300

    added likeRecipe function to module.export

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -7,33 +6,22 @@ const port = 4000;
 const { readRecipes, createRecipe, likeRecipe } = require("./db");
 app.use(cors());
 app.use(express.json());
-=======
-const express = require("express")
-var cors = require("cors")
-const app = express()
-app.use(cors())
-const port = 3000
-
-const { readRecipes, createRecipe, deleteRecipe } = require("./db")
-
-app.use(express.json())
->>>>>>> a5b576ca3c246e5f66358bbe4699b12b4f765deb
 
 app.get("/", async (req, res) => {
-  const recipes = await readRecipes()
-  const response = { data: recipes }
-  res.send(response)
-})
+  const recipes = await readRecipes();
+  const response = { data: recipes };
+  res.send(response);
+});
 
 app.post("/recipe", async (req, res) => {
-  await createRecipe(req.body)
-  res.sendStatus(200)
-})
+  await createRecipe(req.body);
+  res.sendStatus(200);
+});
 
 app.delete("/recipe/:recipe_id", async (req, res) => {
-  await deleteRecipe(req.params.recipe_id)
-  res.sendStatus(200)
-})
+  await deleteRecipe(req.params.recipe_id);
+  res.sendStatus(200);
+});
 
 app.post("/toggle-favorite/:id", async (req, res) => {
   await likeRecipe(req.params.id);
@@ -41,5 +29,5 @@ app.post("/toggle-favorite/:id", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+  console.log(`Server is listening on port ${port}`);
+});
