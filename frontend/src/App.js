3de808508cbb06fbf1f8f8afcef8eda85787b96c
kaisadiakhate <kaisa.diakhate@awacademy.training>
2022-09-21 14:53:44 +0300

    added heading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
-import React from "react"
-import PostList from "./PostList"
-import Container from "@mui/material/Container"
-import Fab from "@mui/material/Fab"
-import AddIcon from "@mui/icons-material/Add"
+import React from "react";
+import PostList from "./PostList";
+import Container from "@mui/material/Container";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
 
-export const ApiUrlContext = React.createContext("localhost")
+export const ApiUrlContext = React.createContext("localhost");
 
-const defaultApiUrl = "http://34.245.57.188:4000"
+const defaultApiUrl = "http://34.245.57.188:4000";
 function App() {
-  const [apiUrl] = React.useState(defaultApiUrl)
+  const [apiUrl] = React.useState(defaultApiUrl);
   //const [apiUrlPrompt, setApiUrlPrompt] = React.useState(defaultApiUrl)
-  const [showNewDialog, setShowNewDialog] = React.useState(false)
+  const [showNewDialog, setShowNewDialog] = React.useState(false);
 
   return (
     <div className="App">
+      <h1>This is a heading</h1>
       <Container fixed>
         <ApiUrlContext.Provider value={apiUrl}>
           <PostList
@@ -23,7 +24,7 @@ function App() {
         </ApiUrlContext.Provider>
         <Fab
           onClick={() => {
-            setShowNewDialog(true)
+            setShowNewDialog(true);
           }}
           color="primary"
           aria-label="add"
@@ -37,7 +38,7 @@ function App() {
         </Fab>
       </Container>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
