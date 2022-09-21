aa8701b6b031029a4ec4838f9fa39e3d64d9be0b
jantsigit <jan.lohva@gmail.com>
2022-09-21 14:24:08 +0300

    fronttia muokattu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,20 @@
-import React from 'react';
-import PostList from './PostList';
-import Container from '@mui/material/Container';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import React from "react"
+import PostList from "./PostList"
+import Container from "@mui/material/Container"
+import Fab from "@mui/material/Fab"
+import AddIcon from "@mui/icons-material/Add"
 
-export const ApiUrlContext = React.createContext('localhost');
+export const ApiUrlContext = React.createContext("localhost")
 
-const defaultApiUrl = process.env.REACT_APP_API_URL;
-
+const defaultApiUrl = "http://34.245.57.188:4000"
 function App() {
-  const [apiUrl, setApiUrl] = React.useState(defaultApiUrl);
-  const [apiUrlPrompt, setApiUrlPrompt] = React.useState(defaultApiUrl);
-  const [showNewDialog, setShowNewDialog] = React.useState(false);
+  const [apiUrl] = React.useState(defaultApiUrl)
+  //const [apiUrlPrompt, setApiUrlPrompt] = React.useState(defaultApiUrl)
+  const [showNewDialog, setShowNewDialog] = React.useState(false)
 
   return (
     <div className="App">
       <Container fixed>
-        <Box display="flex" marginTop={1} marginBottom={1}>
-          <TextField
-            fullWidth
-            value={apiUrlPrompt}
-            onChange={(e) => {
-              setApiUrlPrompt(e.target.value);
-            }}
-          />
-          <IconButton onClick={() => setApiUrl(apiUrlPrompt)}>
-            <ArrowForwardIosIcon />
-          </IconButton>
-        </Box>
         <ApiUrlContext.Provider value={apiUrl}>
           <PostList
             show={showNewDialog}
@@ -40,12 +23,12 @@ function App() {
         </ApiUrlContext.Provider>
         <Fab
           onClick={() => {
-            setShowNewDialog(true);
+            setShowNewDialog(true)
           }}
           color="primary"
           aria-label="add"
           sx={{
-            position: 'fixed',
+            position: "fixed",
             bottom: 20,
             right: 20,
           }}
@@ -54,7 +37,7 @@ function App() {
         </Fab>
       </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
